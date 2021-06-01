c35541357a4c0a3f0d7d60a127d112e535bab5b7
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-06-01 23:28:18 +0300

    Change routes for publish

--- TeamManagementMobile/src/app/constants/apiRoutes.ts
+++ TeamManagementMobile/src/app/constants/apiRoutes.ts
@@ -1,5 +1,7 @@
 export class Routes {
-  private static API_BASE_URL: string = "http://10.0.2.2:5000/v1";
+  // private static API_BASE_URL: string = "http://10.0.2.2:5000/v1";
+  private static API_BASE_URL: string =
+    "https://teammanagement.azurewebsites.net/v1";
   public static Login: string = `${Routes.API_BASE_URL}/auth/login`;
   public static UserInfo: string = `${Routes.API_BASE_URL}/auth/user-info`;
   public static RawArticles: string = `${Routes.API_BASE_URL}/articles`;
